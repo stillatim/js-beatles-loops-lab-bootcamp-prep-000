d27c1fc1b6a35ae561572d8663b4885ec8bb04e2
Justin Evans <evansjustindavid@gmail.com>
2020-02-07 21:54:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function johnLennonFacts(facts) {
   var array = []; 
   var i = 0;
   while (i < facts.length) {
-    array.push(facts[i] + ` !!!`); 
+    array.push(facts[i] + `!!!`); 
     i++;
   }
   return array 
